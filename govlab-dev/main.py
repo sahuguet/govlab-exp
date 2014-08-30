2699dd819f2720b784127b2d78af4f18f0898bfc
Arnaud Sahuguet <sahuguet@Arnauds-MacBook-Pro.local>
2014-08-30 00:52:05 -0400

    lots of stuff

--- govlab-dev/main.py
+++ govlab-dev/main.py
@@ -1,20 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from datetime import datetime
@@ -22,6 +5,13 @@ from datetime import timedelta
 import logging
 
 import webapp2
+import os
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class UserProfile(ndb.Model):
   """Models the profile (JSON) of an individual user."""
@@ -32,64 +22,6 @@ class UserProfile(ndb.Model):
   def query_profile(cls, ancestor_key):
       return cls.query(ancestor=ancestor_key).get()
 
-class UserSnippet(ndb.Model):
-    """Models a user weekly snippet."""
-    content = ndb.TextProperty()
-
-class SnippetHandler(webapp2.RequestHandler):
-    SNIPPET_START_DATE = datetime(2014, 1, 1) # First Wed of 2014 
-
-    @staticmethod
-    def displayWeekRange(week):
-        start_date = SnippetHandler.SNIPPET_START_DATE + timedelta(days=7*week)
-        end_date = SnippetHandler.SNIPPET_START_DATE + timedelta(days=7*(week+1)-1)
-        return "{:%d, %b %Y (%A)}".format(start_date) + " to " + "{:%d, %b %Y (%A)}".format(end_date)
-
-    def get(self, _user=None, _week=None):
-        
-        logging.info("_user = %s, _week=%s" % (_user, _week))
-        logging.info("Current user: " + users.get_current_user().email())
-        edit = False
-        user = None
-        week = None
-        if _user == None:
-            user = users.get_current_user().email()
-            edit = True
-        else:
-            user = _user
-            if _user == users.get_current_user().email():
-                edit = True
-
-        if _week == None:
-            week = (datetime.today() - SnippetHandler.SNIPPET_START_DATE).days / 7
-        else:
-            week = int(_week)
-        logging.info("+++++ The user is %s." % user)
-        snippet = UserSnippet.get_by_id(id=week, parent=ndb.Key("User", user))
-        if snippet:
-            snippet_data = snippet.content
-        else:
-            snippet_data = "N/A"
-
-        nav = """<div><a href="/snippet/%s/%d">Prev</a>&nbsp;<a href="/snippet/%s/%d">Next</a></div>"""% (user, week-1, user, week+1)
-
-        if edit:
-            self.response.out.write("""Showing snippet for user %s and %s:
-            %s
-            <form action="/snippet/%s/%d" method="POST">
-            <div><textarea name="snippet_data" rows="10" cols="60">%s</textarea></div>
-            <div><input type="submit" value="Update Snippet"></div>
-            </form>""" % (user, SnippetHandler.displayWeekRange(week), nav, user, week, snippet_data))
-        else:
-            self.response.out.write("Showing snippet for user %s and %s:%s<pre>%s</pre>" % (user, SnippetHandler.displayWeekRange(week), nav, snippet_data))
-
-    def post(self, user, week):
-        logging.info("Inside POST")
-        snippet_data = self.request.get('snippet_data')
-        snippet = UserSnippet(parent=ndb.Key("User", user), id=int(week), content=snippet_data)
-        snippet.put()
-        self.redirect('/snippet/%s/%s' % (user, week))
-            
 class UserProfileHandler(webapp2.RequestHandler):
     def get(self):
         the_other_user = self.request.get('user')
@@ -153,12 +85,21 @@ class AcademyHandler(webapp2.RequestHandler):
             
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
+class DashboardHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/dashboard.html')
+        user = users.get_current_user()
+        if user:
+            template_values = { 'user': user.email(), 'logout': users.create_logout_url('/')}
+            self.response.out.write(template.render(template_values))
+        else:
+            greeting = ('Hi stranger to the GovLab Academy brochureware. (<a href="%s">Sign in if are part of the Academy community</a>).' %
+                        users.create_login_url('/dashboard'))
+            self.response.out.write('<html><body>%s</body></html>' % greeting)
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/academy', AcademyHandler),
     ('/profile', UserProfileHandler),
-    (r'/snippet/(.+)/(.+)', SnippetHandler),
-    (r'/snippet/(.+)', SnippetHandler),
-    (r'/snippet/', SnippetHandler),
-    
+    ('/dashboard', DashboardHandler),
 ], debug=True)
